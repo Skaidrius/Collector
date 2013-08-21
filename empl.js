d3d394f335a44284e3610dadcc7de8fea159203e
Skaidrius <skaidrius@gmail.com>
2013-08-21 09:45:16 +0300

    Create empl.js
    Adds to array from simple list.

---
+++ empl.js
@@ -0,0 +1,4 @@
+//
+var temp = "First Person, Second Person, Third Person, Fourth Person, Fifth Person, Sixth Person, Seventh Person, Eight Person, Ninth Person, Tenth Person;
+var empl = new Array();
+empl = temp.split(',');
